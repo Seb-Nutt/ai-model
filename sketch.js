08dcf989e6f5a052e909ad8b82ab6611cbbe0252
Seb-Nutt <nuttsebastian@gmail.com>
2026-05-08 12:12:42 -0600

    Worked on the nodes and coloring system further

--- sketch.js
+++ sketch.js
@@ -6,9 +6,16 @@
 // - describe what you did to take this project "above and beyond"
 
 let brainNodes = [];
-//keep adding colors
-//ajust the last nodes
+let regionIDs = {
+  brainStem: 0,
+  cerebellum: 1,
+  occipital: 2,
+  parietal: 3,
+  frontal: 4,
+  temporal: 5
+};
 let regionColors = [['grey'], ['red'], ['yellow'], ['green'], ['cyan'], ['blue']];
+let regionNodes = [[],[],[],[],[],[]];
 let nodePositions;
 let rotationAngle = 0;
 let selectingLobe = false;
@@ -27,7 +34,7 @@ class BrainNode{
   connect(otherNode){
     //use a scale
     
-    if (dist(this.x,this.y,this.z,otherNode.x,otherNode.y,otherNode.z) < this.NODE_CONNECTION_RANGE){
+    if (dist(this.x,this.y,this.z,otherNode.x,otherNode.y,otherNode.z) < this.NODE_CONNECTION_RANGE && (this.regionColor === otherNode.regionColor || !selectingLobe)){
       line(this.x,this.y,this.z,otherNode.x,otherNode.y,otherNode.z);
     }
   }
@@ -54,14 +61,14 @@ function setup() {
   for (node of nodePositions){
     //create the nodes and classify their region
     brainNodes.push(new BrainNode(node[0],node[1],node[2], node[3]));
+    regionNodes[node[3]].push([node[0],node[1],node[2]]);
 
     //Mirror the node if it is not centered
     if (node[2] !== 0){
       brainNodes.push(new BrainNode(node[0],node[1],node[2]*-1, node[3]));
+      regionNodes[node[3]].push([node[0],node[1],node[2]*-1]);
     }
   }
-
-  
 }
 
 function draw() { 
@@ -71,7 +78,7 @@ function draw() {
 
 
 function drawBrain(){
-  rotationAngle %= 6;
+  rotationAngle %= 2*PI;
   //rotate the brain if a lobe is not being selected
   if (selectingLobe){
     //rotate the brain back into position
@@ -101,6 +108,20 @@ function drawBrain(){
         node.connect(otherNode);
       }
     }
+
+    if (selectingLobe){
+      
+      for (let region = 0; region < regionNodes.length; region++){
+        //maybe add a way to determine the border
+        fill(regionColors[region]);
+        beginShape(TRIANGLE_STRIP);
+        for (let node of regionNodes[region]){
+          vertex(node[0],node[1],node[2]);
+        }
+        endShape(CLOSE);
+      }
+    }
+
   }
 
 
